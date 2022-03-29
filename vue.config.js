bcc3b0bfe4f3eab6aae7ddb546da4b58498a2a0e
eddieliu <eddie.liu@hellosanta.com.tw>
2022-03-29 17:01:04 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: `https://eddieliu58.github.io/vue3`,
+  publicPath: "https://eddieliu58.github.io/vue3",
   devServer: {
     proxy: "https://tour.klcg.gov.tw/data",
   },
