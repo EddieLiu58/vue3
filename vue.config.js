70d65888e0e80be44a7bbdebec72e7285a083978
eddie <kyggiik4689@gmail.com>
2022-03-29 23:31:31 +0800

    proxy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 module.exports = {
   publicPath: process.env.VUE_APP_ENV === "production" ? "/vue3/" : "/",
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "https://tour.klcg.gov.tw/data",
+        pathRewrite: { "^/api": "" },
+        ws: true,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   filenameHashing: false,
   configureWebpack: {
     devtool: "source-map",
