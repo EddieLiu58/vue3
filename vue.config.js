a20e75d8bdecac7e0865ad0e8efa540b3fd1404e
eddieliu <eddie.liu@hellosanta.com.tw>
2022-03-29 17:32:02 +0800

    env

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 module.exports = {
-  publicPath: "/vue3/",
-  devServer: {
-    proxy: "https://tour.klcg.gov.tw/data",
-  },
+  publicPath: process.env.VUE_APP_ENV === "production" ? "/vue3/" : "/",
+  // devServer: {
+  //   proxy: "https://tour.klcg.gov.tw/data",
+  // },
   filenameHashing: false,
   configureWebpack: {
     devtool: "source-map",
