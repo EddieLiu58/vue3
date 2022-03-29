2d1b0fd93cb75bc4a70746a17ccde7a45d42b19e
eddie <kyggiik4689@gmail.com>
2022-03-29 20:48:42 +0800

    cors error

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 module.exports = {
   publicPath: process.env.VUE_APP_ENV === "production" ? "/vue3/" : "/",
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "https://tour.klcg.gov.tw/data",
-        pathRewrite: { "^/api": "" },
-        changeOrigin: true,
-        secure: false,
-      },
+  proxy: {
+    "/api": {
+      target: "https://tour.klcg.gov.tw/data",
+      pathRewrite: { "^/api": "" },
+      changeOrigin: true,
+      secure: false,
     },
   },
   filenameHashing: false,
