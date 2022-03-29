d111bf82d31caf73670987cc89dbfbde982c99b2
eddieliu <eddie.liu@hellosanta.com.tw>
2022-03-29 16:51:04 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   devServer: {
     proxy: "https://tour.klcg.gov.tw/data",
   },
+  filenameHashing: false,
+  configureWebpack: {
+    devtool: "source-map",
+  },
+  css: {
+    extract: true,
+  },
 };
