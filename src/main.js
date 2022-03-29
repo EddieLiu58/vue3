870db01ae6f6ee241fa888ea5d82c33e848623df
EddieLiu58 <EddieLiu58@users.noreply.github.com>
2022-03-29 08:43:40 +0000

    set up action 3c5cd9d88d154740fda19dd94a57b2d00a9c5133

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import axios from "axios";
-const app = createApp(App);
-app.config.globalProperties.axios = axios;
-axios.defaults.withCredentials = true;
-app.mount("#app");
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  devServer: {
-    proxy: "https://tour.klcg.gov.tw/data",
-  },
-};
