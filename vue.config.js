668bf944d894a65b1af081b187512cbea19d3d1d
eddie <kyggiik4689@gmail.com>
2022-03-29 20:51:23 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 module.exports = {
   publicPath: process.env.VUE_APP_ENV === "production" ? "/vue3/" : "/",
-  proxy: {
-    "/api": {
-      target: "https://tour.klcg.gov.tw/data",
-      pathRewrite: { "^/api": "" },
-      changeOrigin: true,
-      secure: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "https://tour.klcg.gov.tw/data",
+        pathRewrite: { "^/api": "" },
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   filenameHashing: false,
